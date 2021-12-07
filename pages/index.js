0cd91d95e862183f8b903c0229c4a2b402c5241b
Craig Blagg <craigblagg@Craigs-Mac-mini.lan>
2021-12-06 22:07:47 -0500

    feat(text): Ensure element for binding editor to

--- pages/index.js
+++ pages/index.js
@@ -13,11 +13,11 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <span>Welcome to </span><a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          <span>Get started by editing{' '}</span>
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -58,7 +58,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          <span>Powered by{' '}</span>
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
